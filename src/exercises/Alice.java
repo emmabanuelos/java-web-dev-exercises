4f25602e696898477ca36d043f2f5b87b418699a
Emma Banuelos <emmabanuelos@icloud.com>
2019-11-07 12:35:53 -0600

    Finished studio 1 without bonus

---
+++ src/exercises/Alice.java
@@ -0,0 +1,30 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Alice {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("Hello, what is your name:");
+//
+//
+//        String userName = input.nextLine();
+//
+//        System.out.println("Hello " + name);
+//    }
+
+//    public static String getName(String[] args) {
+//
+//        if (name.equals("Alice")) {
+//            return "Hello Alice!";
+//        } else if (name.equals("Emma")) {
+//            return "Hey! You're not Alice!";
+//        } else if (name.equals(" ")) {
+//            return "Hello? Can you hear me?";
+//        } else {
+//            return "Have a nice day!";
+//        }
+//    }
+}
+
